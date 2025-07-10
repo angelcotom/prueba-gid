0741d0f39dcabda6588b67ad4f7dc939ae8c7f30
jonan <heber2025sandoval@gmail.com>
2025-07-09 19:27:55 -0600

    EDICION 4

--- init.py
+++ init.py
@@ -1,3 +1,17 @@
 print("hola mundo")
 print("queso")
 print("QUE ONDA")
+
+from collections import deque
+class Recepcion:
+    def __init__(self):
+        self.cola_pacientes = deque()
+    def agregar_paciente(self, nombre):
+        self.cola_pacientes.append(noombre)
+        print(f"[RECEPCION]Paciente agregado:{nombre}")
+    def atender_paciente(self):
+        if self.cola_pacientes:
+        atendido= self.cola_pacientes.popleft()
+        print(f"[RECEPCION] Paciente atentido;{atendido}")
+        else:
+
