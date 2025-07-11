f0a513bdd543a2ccaf3d0c6e434930b4cc31cc8d
jonan <heber2025sandoval@gmail.com>
2025-07-11 14:36:24 -0600

    PRIMERA PARTE JONATHAN

--- init.py
+++ init.py
@@ -7,12 +7,27 @@ class Recepcion:
     def __init__(self):
         self.cola_pacientes = deque()
     def agregar_paciente(self, nombre):
-        self.cola_pacientes.append(noombre)
+        self.cola_pacientes.append(nombre)
         print(f"[RECEPCION]Paciente agregado:{nombre}")
     def atender_paciente(self):
         if self.cola_pacientes:
-        atendido= self.cola_pacientes.popleft()
-        print(f"[RECEPCION] Paciente atentido;{atendido}")
+            atendido = self.cola_pacientes.popleft()
+            print(f"[RECEPCION] Paciente atentido:{atendido}")
         else:
-        print()
+            print("[RECEPCION] No hay pacientesen la cola")
+    def mostrar_cola(self):
+        if self.cola_pacientes:
+            print("[RECEPCION] Pacientes en espera:")
+            for paciente in self.cola_pacientes:
+                print(f"{paciente}")
+            else:
+                print("[RECPECION] La cola esta vacia")
+if_name_=="":
+    r=Recepcion()
+    r.agregar_paciente("JUAN")
+    r.agregar_paciente("LUIS")
+    r.mostrar_cola()
+    r.atender_paciente()
+    r.mostrar_cola()
+
 
