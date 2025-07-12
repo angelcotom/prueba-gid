2ea03109a80d5bef6b8b337946c5347b5bee5562
Brand <vasbran111@gmail.com>
2025-07-11 21:16:06 -0500

    .

---
+++ Actividad 05.py
@@ -0,0 +1,13 @@
+def RegEstudiante ():
+    nombres = int(input("Ingrese el nombre"))
+    carne = int(input("Ingrese el numero de carne"))
+    carrera = int(input("Ingrese la carrera"))
+    Nota = int(input("Ingrese la nota"))
+
+'''def MostrarListadoEstudiante ():'''
+
+'''def BuscarEstudianteNoCarne ():'''
+
+'''def CalcularPromNotasEstudiante():'''
+
+print("BIENVENIDO AL REGISTRO DE ESTUDIANTES")
